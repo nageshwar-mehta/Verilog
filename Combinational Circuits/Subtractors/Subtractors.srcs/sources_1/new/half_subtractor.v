ce3ec6dfbac1df61fb76083ad057f5f499119cd6
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-14 20:39:51 +0530

    Add initial files for Half Subtractor implementation and project setup

---
+++ Combinational Circuits/Subtractors/Subtractors.srcs/sources_1/new/half_subtractor.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.07.2025 20:39:26
+// Design Name: 
+// Module Name: half_subtractor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_subtractor(
+
+    );
+endmodule

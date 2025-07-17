c1ea2f8cc02be73fb17c1ed566c6871590565766
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-18 05:08:36 +0530

    Refactor D flip flop simulation files and update project configurations

---
+++ Sequential Circuits/JK flip flop/JK flip flop.srcs/sources_1/new/JK1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.07.2025 05:07:55
+// Design Name: 
+// Module Name: JK1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module JK1(
+
+    );
+endmodule

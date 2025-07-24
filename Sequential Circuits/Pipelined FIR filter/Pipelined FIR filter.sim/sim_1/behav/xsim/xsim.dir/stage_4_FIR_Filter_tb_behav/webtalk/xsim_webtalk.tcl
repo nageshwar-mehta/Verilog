85d6ce2cc6ecc1bdc1e10c4fe67775439996a933
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-24 22:15:24 +0530

    Run synsthesis successfull

--- Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/xsim_webtalk.tcl
+++ Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Jul 24 16:43:48 2025" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Jul 24 22:14:57 2025" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "351d6f81-0428-4cc3-9a7a-28ba5a9981ff" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "1de3ea785104412985f344eefd690d7f" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "9" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 5600U with Radeon Graphics         " -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "120 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "8664_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1725294598 -regid "212824581_0_0_168" -xml C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "8656_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1148715747 -regid "212824581_0_0_168" -xml C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/nages/verilog_projects/Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.sim/sim_1/behav/xsim/xsim.dir/stage_4_FIR_Filter_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate

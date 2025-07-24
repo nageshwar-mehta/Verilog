f263224aa6c4f50170a55ec54d22bd073c6afe46
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-24 06:43:44 +0530

    Implement stage-wise processing in Pipelined FIR filter and add necessary XML configuration files

--- Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.srcs/sources_1/new/stage_4_FIR_Filter.v
+++ Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.srcs/sources_1/new/stage_4_FIR_Filter.v
@@ -1,26 +1,71 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.07.2025 05:48:45
-// Design Name: 
-// Module Name: stage_4_FIR_Filter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+// y[n] = h0.x[n] + h1.x[n-1] + h2.x[n-2] + h3.x[n-3]
+//x: input
+//y: output
+//h: coefficents
+module stage_4_FIR_Filter(clk,reset,x_in,y_out);
+input clk,reset;
+input signed[7:0]x_in;
+output reg signed [15:0]y_out;
 
+//Example coefficients : 
+parameter signed[7:0] h0 = 8'sd1;
+parameter signed[7:0] h1 = -8'sd1;
+parameter signed[7:0] h2 = 8'sd0;
+parameter signed[7:0] h3 = 8'sd2;
 
-module stage_4_FIR_Filter(
+//input registers
+reg signed[7:0]xn1,xn2,xn3;
+reg signed[15:0]p0,p1,p2,p3;
+reg signed[15:0] out_sum1,out_sum2;
 
-    );
+//stage 1 : delay inputs assignment
+always@(posedge clk or reset)begin
+    if(reset)begin
+        xn1<=0;
+        xn2<=0;
+        xn3<=0;
+    end
+    else begin
+        xn1<=x_in;//x_in = x[n] and xn1 = x[n-1]
+        xn2<=xn1;//xn2 = x[n-2] 
+        xn3<=xn2;//xn3 = x[n-3] 
+    end
+end
+//stage 2 : product 
+always@(posedge clk or reset)begin
+    if(reset)begin
+        p0<=0;
+        p1<=0;
+        p2<=0;
+        p3<=0;
+    end
+    else begin
+        p0<=h0*x_in;
+        p1<=h1*xn1;
+        p2<=h2*xn2;
+        p3<=h3*xn3;
+    end
+end
+//stage 3  sum
+always@(posedge clk or reset)begin
+    if(reset)begin
+        out_sum1<=0;
+        out_sum2<=0;
+    end
+    else begin
+        out_sum1 <= p0+p1;
+        out_sum2 <= p2+p3;
+    end
+end
+//stage 4  final sum
+always@(posedge clk or reset)begin
+    if(reset)begin
+        y_out<=0;
+    end
+    else begin
+        y_out<=out_sum1+out_sum2;
+    end
+end
+
 endmodule

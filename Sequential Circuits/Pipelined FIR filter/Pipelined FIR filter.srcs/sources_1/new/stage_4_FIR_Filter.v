270876390ab2ad45b4c0e1c1ca1f61bd4e7efc86
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-24 05:49:39 +0530

    Add project files for Pipelined FIR filter including configuration and source code

---
+++ Sequential Circuits/Pipelined FIR filter/Pipelined FIR filter.srcs/sources_1/new/stage_4_FIR_Filter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.07.2025 05:48:45
+// Design Name: 
+// Module Name: stage_4_FIR_Filter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module stage_4_FIR_Filter(
+
+    );
+endmodule

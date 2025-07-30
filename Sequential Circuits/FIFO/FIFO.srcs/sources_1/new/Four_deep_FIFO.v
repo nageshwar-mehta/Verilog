725060b5b776988ef3a90f3fd3f08c6ecf0d7a99
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-30 23:13:00 +0530

    FIFO code completed

--- Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO.v
+++ Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO.v
@@ -1,9 +1,28 @@
 `timescale 1ns / 1ps
 
 
-module Four_deep_FIFO(data_in,push,pop,fifo_empty,fifo_full,data_out);
+module Four_deep_FIFO(clk,data_in,push,pop,fifo_empty,fifo_full,data_out);
 input [3:0]data_in;
-input push,pop;
-output fifo_empty,fifo_full;
-output[3:0] data_out;
+input push,pop,clk;
+output reg fifo_empty,fifo_full;
+output reg [3:0] data_out;
+
+reg [2:0]counter = 0;
+reg [3:0]fifo_mem[3:0];
+always@(posedge clk)begin
+    if(fifo_empty!=0)data_out <= fifo_mem[counter-1];
+    if(push==1)begin
+        fifo_mem[counter] <=data_in;
+        counter <= counter+1;
+    end
+    if(pop==1)begin
+        counter <= counter-1;
+        fifo_mem[counter] <=0;
+    end
+    if(counter==0) fifo_empty <=0;
+    if(counter==4) fifo_full <=0;
+    
+    
+end
+
 endmodule

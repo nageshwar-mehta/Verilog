779ee5dd85ffc368c93f25b7f4437cb8975ad55f
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-07-30 23:59:00 +0530

    simulation failing

--- Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO.v
+++ Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO.v
@@ -6,23 +6,27 @@ input [3:0]data_in;
 input push,pop,clk;
 output reg fifo_empty,fifo_full;
 output reg [3:0] data_out;
-
+initial begin
+ fifo_empty =0;
+ fifo_full = 0; 
+ end
 reg [2:0]counter = 0;
 reg [3:0]fifo_mem[3:0];
 always@(posedge clk)begin
-    if(fifo_empty!=0)data_out <= fifo_mem[counter-1];
+    data_out <= fifo_mem[counter-1];
     if(push==1)begin
-        fifo_mem[counter] <=data_in;
+        fifo_mem[3] <= fifo_mem[2];
+        fifo_mem[2] <= fifo_mem[1];
+        fifo_mem[1] <= fifo_mem[0];
+        fifo_mem[0] <= data_in;
         counter <= counter+1;
     end
     if(pop==1)begin
         counter <= counter-1;
         fifo_mem[counter] <=0;
     end
-    if(counter==0) fifo_empty <=0;
-    if(counter==4) fifo_full <=0;
-    
-    
+    fifo_empty  = (counter==0);
+    fifo_full = (counter==4);
 end
 
 endmodule
--- Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO_tb.v
+++ Sequential Circuits/FIFO/FIFO.srcs/sources_1/new/Four_deep_FIFO_tb.v
@@ -1,26 +1,115 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 30.07.2025 23:11:09
-// Design Name: 
-// Module Name: Four_deep_FIFO_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module Four_deep_FIFO_tb(
+module Four_deep_FIFO_tb;
 
-    );
+//output ports
+wire fifo_empty,fifo_full;
+wire [3:0] data_out;
+
+//input ports
+reg [3:0]data_in;
+reg push,pop,clk;
+
+//DUT
+Four_deep_FIFO uut(
+    .data_in(data_in),
+    .push(push),
+    .pop(pop),
+    .clk(clk)
+);
+
+//clk initialization
+initial begin
+    clk = 0;
+    forever #5 clk=~clk;//time period = 10s
+end
+
+//settle down test
+initial begin
+#10;
+    repeat(6)begin
+        #10 data_in = 0;
+        #10 push = 1;  
+    end
+end
+
+//Tests
+initial begin 
+//=================PUSH==================//
+    //test 1
+    #10;
+    data_in = 3;
+    push = 1;
+    #10 push = 0;  
+    //test 2
+    #10;
+    data_in = 9;
+    push = 1;
+    #10 push = 0;  
+    //test 3;
+    #10;
+    data_in = 8;
+    push = 1;
+    #10 push = 0;  
+    //test 4;
+    #10;
+    data_in = 6;
+    push = 1;
+    #10 push = 0;  
+    //test 5;
+    #10;
+    data_in = 1;
+    push = 1;
+    #10 push = 0;  
+    
+//===============POP==================//
+    //test 1
+    #10;
+    pop = 1;
+    #10 pop = 0;
+    //test 2
+    #10;
+    pop = 1;
+    #10 pop = 0;
+    
+//==============POP & PUSH============//
+    //test 1
+    #10;        
+    data_in = 6;
+    push = 1;
+    #10 push = 0;   
+    //test 2  
+    #10;        
+    data_in = 1;
+    push = 1; 
+    #10 push = 0;  
+    //test 3 
+    #10;    
+    pop = 1;   
+    #10 pop = 0;
+    //test 4;   
+    #10;        
+    data_in = 8;
+    push = 1;
+    #10 push = 0;     
+    //test 3 
+    #10;     
+    pop = 1; 
+    #10 pop = 0;
+    //test 3 
+    #10;     
+    pop = 1; 
+    #10 pop = 0;
+    //test 3 
+    #10;     
+    pop = 1; 
+    #10 pop = 0;
+    
+    $finish;
+end
+
+initial begin
+$monitor("time: %0t, data_in: %d, push: %b, pop: %b, data_out: %d, fifo_empty: %b, fifo_full: %b",$time,data_in,push,pop,data_out,fifo_empty,fifo_full);
+end
 endmodule

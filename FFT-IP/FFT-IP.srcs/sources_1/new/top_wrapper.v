a1b7aa162fb352fc8bd9457c71cf816550195b07
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-09-02 02:42:10 +0530

    Add initial project files and configurations for FFT-IP

---
+++ FFT-IP/FFT-IP.srcs/sources_1/new/top_wrapper.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.09.2025 18:35:20
+// Design Name: 
+// Module Name: top_wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_wrapper(
+
+    );
+endmodule

737d80c444a0267a1358a1942b47c8442dcda77a
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-09-02 15:53:57 +0530

    Refactor code structure for improved readability and maintainability

--- FFT-IP/FFT-IP.srcs/sources_1/new/top_wrapper.v
+++ FFT-IP/FFT-IP.srcs/sources_1/new/top_wrapper.v
@@ -1,26 +1,30 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.09.2025 18:35:20
-// Design Name: 
-// Module Name: top_wrapper
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module top_wrapper(
 
     );
+    
+    
+    fft_64 FFT_IP (
+  .aclk(aclk),                                                // input wire aclk
+  .aresetn(aresetn),                                          // input wire aresetn
+  .s_axis_config_tdata(s_axis_config_tdata),                  // input wire [7 : 0] s_axis_config_tdata
+  .s_axis_config_tvalid(s_axis_config_tvalid),                // input wire s_axis_config_tvalid
+  .s_axis_config_tready(s_axis_config_tready),                // output wire s_axis_config_tready
+  .s_axis_data_tdata(s_axis_data_tdata),                      // input wire [63 : 0] s_axis_data_tdata
+  .s_axis_data_tvalid(s_axis_data_tvalid),                    // input wire s_axis_data_tvalid
+  .s_axis_data_tready(s_axis_data_tready),                    // output wire s_axis_data_tready
+  .s_axis_data_tlast(s_axis_data_tlast),                      // input wire s_axis_data_tlast
+  .m_axis_data_tdata(m_axis_data_tdata),                      // output wire [63 : 0] m_axis_data_tdata
+  .m_axis_data_tvalid(m_axis_data_tvalid),                    // output wire m_axis_data_tvalid
+  .m_axis_data_tready(m_axis_data_tready),                    // input wire m_axis_data_tready
+  .m_axis_data_tlast(m_axis_data_tlast),                      // output wire m_axis_data_tlast
+  .event_frame_started(event_frame_started),                  // output wire event_frame_started
+  .event_tlast_unexpected(event_tlast_unexpected),            // output wire event_tlast_unexpected
+  .event_tlast_missing(event_tlast_missing),                  // output wire event_tlast_missing
+  .event_status_channel_halt(event_status_channel_halt),      // output wire event_status_channel_halt
+  .event_data_in_channel_halt(event_data_in_channel_halt),    // output wire event_data_in_channel_halt
+  .event_data_out_channel_halt(event_data_out_channel_halt)  // output wire event_data_out_channel_halt
+);
 endmodule

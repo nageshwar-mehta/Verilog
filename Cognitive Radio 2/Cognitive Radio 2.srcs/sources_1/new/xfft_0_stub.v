8253444c688e7743a9de8844db7787b066f7a695
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-09-27 22:22:36 +0530

    cleanup: ignore Vivado/XSim generated files

---
+++ Cognitive Radio 2/Cognitive Radio 2.srcs/sources_1/new/xfft_0_stub.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 27.09.2025 21:22:33
+// Design Name: 
+// Module Name: xfft_0_stub
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module xfft_0_stub(
+
+    );
+endmodule

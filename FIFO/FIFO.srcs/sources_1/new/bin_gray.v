6938ed8cca839b221c5d80a459f8629f6cccb35b
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-09-29 01:25:21 +0530

    update: modify simulation scripts and logs for asynchronous FIFO implementation

--- FIFO/FIFO.srcs/sources_1/new/bin_gray.v
+++ FIFO/FIFO.srcs/sources_1/new/bin_gray.v
@@ -1,7 +1,11 @@
 `timescale 1ns / 1ps
+//input and output width = ADDR_WIDTH-1 : 0 for actual memory address
+//                       = ADDR_WIDTH for wrap around flag : this is used for implementation of ASYNCH FIFO full and empty condition
 
-module bin_gray #(parameter WIDTH=8)(bin_num,gray_num);
-input[WIDTH-1:0] bin_num;
-output[WIDTH-1:0] gray_num;
-assign gray_num = bin_num ^ (bin_num>>1);
+module bin_gray #(parameter ADDR_WIDTH=4)(
+    input [ADDR_WIDTH:0] bin,
+    output [ADDR_WIDTH:0] gray
+);
+    assign gray = bin ^ (bin >> 1);
 endmodule
+

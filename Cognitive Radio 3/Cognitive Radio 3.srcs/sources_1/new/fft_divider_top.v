8496f7375d7f19432d73aa318c85145150662141
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2025-10-26 02:54:33 +0530

    Add utilization report for FFT64pt design in Cognitive Radio 3 project
    - Generated utilization report using Vivado v.2020.2
    - Includes detailed information on slice logic, memory, DSP usage, IO and GT specifics, clocking, specific features, primitives, black boxes, and instantiated netlists.
    - Provides insights into resource utilization percentages and counts for various components.

---
+++ Cognitive Radio 3/Cognitive Radio 3.srcs/sources_1/new/fft_divider_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.10.2025 02:54:10
+// Design Name: 
+// Module Name: fft_divider_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fft_divider_top(
+
+    );
+endmodule

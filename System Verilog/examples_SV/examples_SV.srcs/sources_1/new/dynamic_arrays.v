b163544108430fa4b36b86087222226a71af1947
Nagesh____ <115608397+nageshwar-mehta@users.noreply.github.com>
2026-01-20 23:59:43 +0530

    dynamic arr example

---
+++ System Verilog/examples_SV/examples_SV.srcs/sources_1/new/dynamic_arrays.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.01.2026 23:43:24
+// Design Name: 
+// Module Name: dynamic_arrays
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dynamic_arrays;
+    int dyn_arr1[];
+    initial begin
+        $display("size of dyn_arr1 = %0d", dyn_arr1.size());
+        dyn_arr1 = new[6];
+        $display("size of dyn_arr1 = %0d", dyn_arr1.size());
+    
+        foreach (dyn_arr1[i])
+            dyn_arr1[i] = i*2;
+    
+        $display("elements of dyn_arr1 = %p", dyn_arr1);
+        $finish;
+    end
+
+endmodule
